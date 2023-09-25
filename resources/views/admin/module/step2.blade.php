58219dd69b2b3700868c89e337a69486354fb0b2
aditamairham66 <aditamairham66@gmail.com>
2023-09-25 15:29:25 +0700

    update module

--- resources/views/admin/module/step2.blade.php
+++ resources/views/admin/module/step2.blade.php
@@ -115,7 +115,104 @@
 @endsection
 @push('bottom')
   <script>
+    $(document).on('click', '.btn-plus', function () {
+        var tr_parent = $(this).parent().parent('tr');
+        clone = `
+        <tr>
+            <td>
+                <input 
+                    type='text' name='column[]'
+                    class='column form-control notfocus' 
+                    onclick='showColumnSuggest(this)' 
+                    onKeyUp='showColumnSuggestLike(this)' 
+                    placeholder='Column Name' 
+                    value=''
+                />
+            </td>
+            <td>
+                <input 
+                    type='text' name='name[]' 
+                    class='name form-control notfocus' 
+                    onclick='showNameSuggest(this)' 
+                    onKeyUp='showNameSuggestLike(this)'
+                    placeholder='Field Name' 
+                    value=''
+                />
+            </td>
+            <td>
+                <select class='form-select is_image' name='is_image[]'>
+                    <option value='0'>N</option>
+                    <option value='1'>Y</option>
+                </select>
+            </td>
+            <td>
+                <select class='form-select is_download' name='is_download[]'>
+                    <option value='0'>N</option>
+                    <option value='1'>Y</option>
+                </select>
+            </td>
+            <td>
+                <a href="javascript:void(0)" class="btn btn-icon btn-sm btn-info btn-plus"><i class='fa fa-plus'></i></a>
+                <a href="javascript:void(0)" class="btn btn-icon btn-sm btn-danger btn-delete"><i class='fa fa-trash'></i></a>
+                <a href="javascript:void(0)" class="btn btn-icon btn-sm btn-success btn-up"><i class='fa fa-arrow-up'></i></a>
+                <a href="javascript:void(0)" class="btn btn-icon btn-sm btn-success btn-down"><i class='fa fa-arrow-down'></i></a>
+            </td>
+        </tr>
+        `;
+        tr_parent.after(clone);
+    })
+
+    $(document).mouseup(function (e) {
+        var container = $(".sub");
+        if (!container.is(e.target)
+            && container.has(e.target).length === 0) {
+            container.hide();
+        }
+    });
+
+    $(document).on('click', '.sub li', function () {
+        var v = $(this).text();
+        $(this).parent('ul').prev('input[type=text]').val(v);
+        $(this).parent('ul').remove();
+    })
     
+    $(document).on('click', '.table-display .btn-delete', function () {
+        $(this).parent().parent().remove();
+    })
+
+    $(document).on('click', '.table-display .btn-up', function () {
+        var tr = $(this).parent().parent();
+        var trPrev = tr.prev('tr');
+        if (trPrev.length != 0) {
+
+            tr.prev('tr').before(tr.clone());
+            tr.remove();
+        }
+    })
+
+    $(document).on('click', '.table-display .btn-down', function () {
+        var tr = $(this).parent().parent();
+        var trPrev = tr.next('tr');
+        if (trPrev.length != 0) {
+
+            tr.next('tr').after(tr.clone());
+            tr.remove();
+        }
+    })
+
+    $(document).on('change', '.is_image', function () {
+        var tr = $(this).parent().parent();
+        if ($(this).val() == 1) {
+            tr.find('.is_download').val(0);
+        }
+    })
+
+    $(document).on('change', '.is_download', function () {
+        var tr = $(this).parent().parent();
+        if ($(this).val() == 1) {
+            tr.find('.is_image').val(0);
+        }
+    })
   </script>
 @endpush
 @push('head')
